5d54d3ba9cd86b9a3825600d00361c40c062b593
moyakino <104491816+moyakino@users.noreply.github.com>
2023-10-23 10:34:59 +0900

    Stage.cpp .h 追加

---
+++ Stage.cpp
@@ -0,0 +1,25 @@
+#include "DxLib.h"
+#include "Stage.h"
+
+Stage::Stage() {
+
+	StageX = 0;
+	StageY = 0;
+
+}
+
+Stage::~Stage() {
+
+}
+
+void Stage::Update() {
+
+
+
+}
+
+void Stage::Draw()const {
+
+
+
+}
---
+++ Stage.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class Stage
+{
+private:
+	int StageX;
+	int StageY;
+
+public:
+	Stage();
+	~Stage();
+
+	void Update();
+
+	//•`‰æˆ—
+	void Draw() const;
+
+
+};
